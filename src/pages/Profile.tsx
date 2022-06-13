54c416942cb15bd4788662cf29b853e247eec6aa
Angel Mascaro <angelmascaro@hotmail.com>
2022-06-13 11:39:10 +0200

    profile carga dades axios

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -4,30 +4,32 @@ import '../App.css'
 import { useParams } from 'react-router-dom';
 
 interface User{
-    Username:string
-    Email:string
+    Username:""
+    Email:""
 }
 
 function Profile() {
 
-    useEffect(() => {
-        // getData();
+    
+    const userId = useParams();
+    const [userData, setUserData] = useState<User>();
 
+    useEffect(() => {
+        getData();
       }, []);
 
-    const userId = useParams();
-    // const [userData, setUserData] = useState<User({})
+    const getData = () =>{
+        axios.get("http://localhost:80/pr1/php/api/usuaris/perfil/" + userId.id)
+        .then((r)=>  setUserData(r.data[0]))
+        .catch(error => console.log(error))
+        console.log(userData)
 
-    // const getData = () =>{
-    //     axios.get("http://localhost:80/pr1/php/api/usuaris/perfil/" + userId.id)
-    //     .then((r)=>  setUserData(r.data))
-    //     console.log(userData)
-
-    // }
+    }
 
     return (
         <div className='profile'>
-            {/* <p>{userData?.Email}</p> */}
+            <p>{userData?.Username}</p>
+            <p>{userData?.Email}</p>
 
         </div>
     )
